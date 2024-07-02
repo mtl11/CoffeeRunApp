beb447fb81156acf7bee0c52ea60b13230f18ed1
Matthew Lewis <matthew.tyler.lewis@gmail.com>
2024-07-01 23:37:18 -0700

    improve UI for form

--- App.tsx
+++ App.tsx
@@ -7,14 +7,17 @@ import { useContext, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from 'react';
 import LoginLayout from './components/screens/auth/login/LoginLayout';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 const Stack = createNativeStackNavigator();
 
 function AuthenticatedStack() {
   return (
-      <Stack.Navigator >
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen          
           name='LoginScreen'
           component={LoginLayout}
+          options={{ navigationBarHidden: true}}
+          // navigationOptions: { header: { visible: false } }
         />
       </Stack.Navigator>
   );
--- components/screens/auth/login/LoginForm.tsx
+++ components/screens/auth/login/LoginForm.tsx
@@ -3,30 +3,31 @@ import {
   View,
   Text,
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  ActivityIndicator
 } from "react-native";
 import AuthTextInput from "../../../global/textInputs/AuthTextInput";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-// const [passwordDontMatch, setPasswordDontMatch] = useState(false);
-
-// const [isAuth, setIsAuth] = useState(false);
-// const [visible, setVisible] = useState(true);
-
 const LoginForm = (props: any) =>{
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    // async function authenticateHandler() {
-    //     try {
-    //       // const token = await authenticateUser(email, password);
-    //       setPasswordDontMatch(false);
-    //       // authCTX.authenticate(token);
-    //       AsyncStorage.setItem("email", email);
-    //     } catch (error) {
-    //       setPasswordDontMatch(true);
-    //       setIsAuth(false);
-    //     }
-    //   }
+    const [passwordDontMatch, setPasswordDontMatch] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
+    const [visible, setVisible] = useState(true);
+    async function authenticateHandler() {
+        try {
+          // const token = await authenticateUser(email, password);
+          setPasswordDontMatch(false);
+          // authCTX.authenticate(token);
+        //   AsyncStorage.setItem("email", email);
+            setIsAuth(false);
+        } catch (error) {
+          setPasswordDontMatch(true);
+          setIsAuth(false);
+        }
+      }
+    
 
     return (
     <KeyboardAwareScrollView>
@@ -43,15 +44,15 @@ const LoginForm = (props: any) =>{
         <TouchableOpacity
           style={styles.buttonContainer}
           onPress={() => {
-            // setIsAuth(true);
-            // authenticateHandler();
+            setIsAuth(true);
+            authenticateHandler();
           }}
         >
-          {/* {!isAuth ? (
+          {!isAuth ? (
             <Text style={styles.buttonText}>Login</Text>
           ) : (
             <ActivityIndicator size={22} />
-          )} */}
+          )}
         </TouchableOpacity>
         <View style={styles.newAccountContainer}>
           <Text style={styles.newAccountText}>Don't have an account?</Text>
@@ -68,34 +69,14 @@ const LoginForm = (props: any) =>{
 }
 
 const styles = StyleSheet.create({
-    placeHolderTextColor:{
-        color:"grey"
-    },
     imageContainer: {
         flexDirection: "row",
         justifyContent: "center",
-      },
-      welcomeText: {
-        fontSize: 20,
-        fontFamily: "Rubik-Medium",
       },
       welcomeContainer: {
         alignSelf: "center",
         marginBottom: "10%",
         marginTop: "10%"
-      },
-      stripedImage: {
-        alignSelf: "center",
-        width: 50,
-        height: 26,
-        resizeMode: "contain",
-        
-      },
-      logoImage: {
-        width: 60,
-        height: 45,
-        resizeMode: "contain",
-        marginHorizontal: "1%"
       },
       image: {
         // alignSelf: "center",
@@ -123,7 +104,8 @@ const styles = StyleSheet.create({
         padding: 16,
         marginHorizontal: "8%",
         borderRadius: 12,
-        marginTop: "7%"
+        marginTop: "7%",
+        backgroundColor: "#98FB98",
       },
       buttonTextSignUp: {
         fontFamily: "Rubik-Medium",
